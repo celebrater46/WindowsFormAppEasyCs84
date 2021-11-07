57ef4faa1a9fc5bd228983484ea836fe30fd01c0
hideru <jota.16.stigma.world@gmail.com>
2021-11-07 18:19:58 +0900

    Wrote codes

--- WindowsFormsAppEasyCs84/Form1.cs
+++ WindowsFormsAppEasyCs84/Form1.cs
@@ -12,9 +12,66 @@ namespace WindowsFormsAppEasyCs84
 {
     public partial class Form1 : Form
     {
+        private Ball bl;
+        private int dx, dy;
+
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Timer";
+            this.ClientSize = new Size(250, 100);
+
+            bl = new Ball();
+
+            Point p = new Point(0, 0);
+            Color c = Color.Blue;
+
+            bl.Point = p;
+            bl.Color = c;
+
+            dx = 2;
+            dy = 2;
+
+            Timer tm = new Timer();
+            tm.Interval = 100;
+            tm.Start();
+
+            this.Paint += new PaintEventHandler(FmPaint);
+            tm.Tick += new EventHandler();
         }
+
+        public void FmPaint(Object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Point p = bl.Point;
+            Color c = bl.Color;
+            SolidBrush br = new SolidBrush(c);
+            g.FillEllipse(br, p.X, p.Y, 10, 10);
+        }
+
+        public void TmTick(Object sender, EventArgs e)
+        {
+            Point p = bl.Point;
+            if (p.X < 0 || p.X > this.ClientSize.Width - 10)
+            {
+                dx = -dx;
+            }
+
+            if (p.Y < 0 || p.Y > this.ClientSize.Height - 10)
+            {
+                dy = -dy;
+            }
+            p.X = p.X + dx;
+            p.Y = p.Y + dy;
+
+            bl.Point = p;
+            this.Invalidate();
+        }
+    }
+
+    class Ball
+    {
+        public Color Color;
+        public Point Point;
     }
 }
